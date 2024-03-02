229ec44254764aaea5e1ef6c7333699c87f8213f
githigaurav <gfxshaolin@gmail.com>
2024-03-02 19:00:17 +0530

    app server configured

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import app from './app.js'
 dotenv.config()
 connectDB()
+
+
+app.listen(process.env.PORT || 3001 , ()=>{
+    console.log(`Server is running at PORT ${process.env.PORT}`)
+})
